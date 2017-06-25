14fb32ee6a7a560c1dde6d6fe250dc39ae1b20c1
lumiseven <lumiseven@yahoo.com>
2017-06-26 00:54:52 +0800

    l

--- springmvc4_test/src/main/java/com/lumiseven/test_springMVC4/MyMvcConfig.java
+++ springmvc4_test/src/main/java/com/lumiseven/test_springMVC4/MyMvcConfig.java
@@ -1,16 +1,24 @@
 package com.lumiseven.test_springMVC4;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 import com.lumiseven.test_springMVC4.interceptor.DemoInterceptor;
+import com.lumiseven.test_springMVC4.messageconverter.CustomMessageConverter;
 
 @Configuration
 @ComponentScan("com.lumiseven.test_springMVC4")
@@ -38,6 +46,9 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter{
 		registry.addResourceHandler("/asserts/**").addResourceLocations("classpath:/asserts/");
 	}
 
+	/*
+	 * using interceptor
+	 */
 	@Bean
 	public DemoInterceptor demoInterceptor(){
 		return new DemoInterceptor();
@@ -46,5 +57,46 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter{
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(demoInterceptor());
+	}
+	
+	/*
+	 * using viewController to substitute simple forward to jsp file
+	 * @see org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter#addViewControllers(org.springframework.web.servlet.config.annotation.ViewControllerRegistry)
+	 */
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/index").setViewName("/index");
+		registry.addViewController("/toUpload").setViewName("/upload");
+		registry.addViewController("/converter").setViewName("/converter");
+	}
+	
+	@Override
+	public void configurePathMatch(PathMatchConfigurer configurer) {
+		configurer.setUseSuffixPatternMatch(false);
+	}
+	
+	/*
+	 * add file upload support
+	 */
+	@Bean
+	public MultipartResolver multipartResolver(){
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setMaxUploadSize(100000);
+		return multipartResolver;
+	}
+	
+	/*
+	 * add Bean name customMessageConverter
+	 * override extendMessageConverters to add one custom HttpMessageConverter
+	 * (override configureMessageConverters method to override HttpMessageConverter which spring registried default)
+	 */
+	@Bean
+	public CustomMessageConverter converter(){
+		return new CustomMessageConverter();
+	}
+	
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(converter());
 	}
 }
--- springmvc4_test/src/main/java/com/lumiseven/test_springMVC4/TestController.java
+++ springmvc4_test/src/main/java/com/lumiseven/test_springMVC4/TestController.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestController {
 
-	@RequestMapping("/index")
-	public String testMvc(){
-		return "index";
-	}
+	/*
+	 * using ViewController registry viewController and setViewName
+	 */
+//	@RequestMapping("/index")
+//	public String testMvc(){
+//		return "index";
+//	}
 }

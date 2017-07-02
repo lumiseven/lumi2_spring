9fafca517bb97df4abab2fa70e6a479b12de0e00
lumiseven <lumiseven@yahoo.com>
2017-07-02 22:33:56 +0800

    l

---
+++ jparesttest/src/main/java/com/lumiseven/springdatajparesttest/JparesttestApplication.java
@@ -0,0 +1,12 @@
+package com.lumiseven.springdatajparesttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JparesttestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JparesttestApplication.class, args);
+	}
+}
